aa6b48c5c097a2a9db12a0178666ac00c575ab6f
Rom1987 <maks3564195@gmail.com>
2022-02-18 13:23:26 +0300

    make it better

--- django_rest_framework/settings.py
+++ django_rest_framework/settings.py
@@ -133,8 +133,20 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+# STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # if DEBUG:
 #     STATICFILES_DIRS = [STATIC_DIR]
