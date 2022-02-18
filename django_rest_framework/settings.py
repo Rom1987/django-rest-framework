29e99e9d297f6fe5b1371eff81e94b9df924db27
Rom1987 <maks3564195@gmail.com>
2022-02-18 12:47:21 +0300

    make it better

--- django_rest_framework/settings.py
+++ django_rest_framework/settings.py
@@ -133,12 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'mysite/static'),
-# ]
+# STATICFILES_DIRS = [STATIC_DIR]
 
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
