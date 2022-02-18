4728610d8f751972378f7dc50ac4196106247b50
Rom1987 <maks3564195@gmail.com>
2022-02-18 13:00:34 +0300

    make it better

--- django_rest_framework/settings.py
+++ django_rest_framework/settings.py
@@ -133,7 +133,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 # STATIC_DIR = os.path.join(BASE_DIR, 'static')
 # if DEBUG:
