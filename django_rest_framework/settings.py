ec5caaa65a7f86b24ff24a8fb6908bc50ce20c9a
Rom1987 <maks3564195@gmail.com>
2022-02-23 16:59:10 +0300

    20.02.2022 setting cors

--- django_rest_framework/settings.py
+++ django_rest_framework/settings.py
@@ -307,15 +307,15 @@ CORS_ORIGIN_WHITELIST = (
     'http://127.0.0.1:5500',
 )
 # пишем каким доменам хотим давать доступ к backend
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:5500",
-#     "http://127.0.0.1:5500",
-#     "http://localhost:8080",
-#     "http://127.0.0.1:8000",
-#     "http://localhost:63342",
-#     'http://127.0.0.1:63342',
-#     'https://django--rest--framework.herokuapp.com',
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+    "http://localhost:8080",
+    "http://127.0.0.1:8000",
+    "http://localhost:63342",
+    'http://127.0.0.1:63342',
+    'https://django--rest--framework.herokuapp.com',
+]
 # CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
 # CORS_ALLOW_CREDENTIALS = True
 # CORS_ALLOWED_ORIGINS = [
