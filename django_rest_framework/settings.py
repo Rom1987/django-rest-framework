87a52846771afdb436b1f14746030dcfa5e23550
Rom1987 <maks3564195@gmail.com>
2022-02-23 18:48:11 +0300

    20.02.2022 setting cors

--- django_rest_framework/settings.py
+++ django_rest_framework/settings.py
@@ -28,11 +28,8 @@ SECRET_KEY = '41ki!s3up2pq68rd4c18d-5&hkn+bh+z#bb-ytpj!+e+$jffiu'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
-MIDDLEWARE_CLASSES = (
-    'corsheaders.middleware.CorsMiddleware',
-    'django.middleware.common.CommonMiddleware',
-)
+ALLOWED_HOSTS = ["127.0.0.1"]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -293,14 +290,14 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 
 )
-
+CORS_ALLOW_ALL_ORIGINS = True
 # пишем каким доменам хотим давать доступ к backend
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "http://localhost:8080",
-    "http://127.0.0.1:8000",
-    "http://localhost:63342",
-    'http://127.0.0.1:63342',
-    'https://django--rest--framework.herokuapp.com',
-]
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5500",
+#     "http://127.0.0.1:5500",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:8000",
+#     "http://localhost:63342",
+#     'http://127.0.0.1:63342',
+#     'https://django--rest--framework.herokuapp.com',
+# ]
