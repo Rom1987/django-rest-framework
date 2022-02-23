9f0270c774a561317b561ed3eb221f34a2cf6caf
Rom1987 <maks3564195@gmail.com>
2022-02-23 14:52:38 +0300

    20.02.2022 setting cors

--- django_rest_framework/settings.py
+++ django_rest_framework/settings.py
@@ -298,24 +298,25 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
 
 )
-CORS_ALLOW_CREDENTIALS = True
 # пишем каким доменам хотим давать доступ к backend
+# CORS_ALLOWED_ORIGINS = [
+#     "http://localhost:5500",
+#     "http://127.0.0.1:5500",
+#     "http://localhost:8080",
+#     "http://127.0.0.1:8000",
+#     "http://localhost:63342",
+#     'http://127.0.0.1:63342',
+#     'https://django--rest--framework.herokuapp.com'
+# ]
+CORS_ALLOW_ALL_ORIGINS = True  # If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5500",
+    'http://localhost:5500',
     "http://127.0.0.1:5500",
-    "http://localhost:8080",
-    "http://127.0.0.1:8000",
-    "http://localhost:63342",
-    'http://127.0.0.1:63342',
-    'https://django--rest--framework.herokuapp.com'
-]
-CORS_ALLOW_METHODS = [
-'DELETE',
-'GET',
-'OPTIONS',
-'PATCH',
-'POST',
-'PUT',
+]  # If this is used, then not need to use `CORS_ALLOW_ALL_ORIGINS = True`
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    'http://localhost:5500',
+    "http://127.0.0.1:5500",
 ]
 CORS_ALLOW_HEADERS = [
     'accept',
