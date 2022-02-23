68fbc21b3609eff005836eea2457425692f71149
Rom1987 <maks3564195@gmail.com>
2022-02-23 20:57:20 +0300

    23.02.2022 .venv deploy pythonanywhere

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\Users\maks2\Desktop\Programming\Python\django-sites\django_rest_framework\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
