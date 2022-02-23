0ef8c14151347fa3665428faa24d12db989fe9bc
Rom1987 <maks3564195@gmail.com>
2022-02-23 18:57:51 +0300

    20.02.2022 setting cors

--- django_rest_framework/settings.py
+++ django_rest_framework/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '41ki!s3up2pq68rd4c18d-5&hkn+bh+z#bb-ytpj!+e+$jffiu'
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "127.0.0.1",
+    # "127.0.0.1",
     "django--rest--framework.herokuapp.com"
 ]
 
