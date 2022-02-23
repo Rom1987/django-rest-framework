56c966a95893eeb40fe46141abc5e44e3d2f6de5
Rom1987 <maks3564195@gmail.com>
2022-02-23 16:04:44 +0300

    20.02.2022 setting cors

--- django_rest_framework/settings.py
+++ django_rest_framework/settings.py
@@ -66,6 +66,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    'movies.middleware.corsMiddleware',
 ]
 
 ROOT_URLCONF = 'django_rest_framework.urls'
