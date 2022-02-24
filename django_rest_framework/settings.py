6b403869d8b8bc8e597eefd64f4c0ae192315373
Rom1987 <maks3564195@gmail.com>
2022-02-24 13:21:42 +0300

    23.02.2022 settings cors heroku

--- django_rest_framework/settings.py
+++ django_rest_framework/settings.py
@@ -296,10 +296,10 @@ AUTHENTICATION_BACKENDS = (
 # CORS_ALLOW_ALL_ORIGINS = True
 # пишем каким доменам хотим давать доступ к backend
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5500",
-    "http://127.0.0.1:5500",
-    "http://localhost:8080",
-    "http://127.0.0.1:8000",
+    # "http://localhost:5500",
+    # "http://127.0.0.1:5500",
+    # "http://localhost:8080",
+    # "http://127.0.0.1:8000",
     'https://django--rest--framework.herokuapp.com',
 ]
 CORS_ALLOW_CREDENTIALS = False
